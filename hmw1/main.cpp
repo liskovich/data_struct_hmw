8d3764762d862644e11bd4d22b1591beed7e50f1
liskovich <ricardsliskovskis@gmail.com>
2024-01-12 16:53:12 +0200

    complete: hmw1

---
+++ hmw1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "**********************************" << endl;
+  cout << "*    Programming Assignment 1    *" << endl;
+  cout << "*        Data Structures         *" << endl;
+  cout << "*   Author: Ricards Liskovskis   *" << endl;
+  cout << "*    Due Date: September, 7th    *" << endl;
+  cout << "**********************************" << endl;
+  return 0;
+}
