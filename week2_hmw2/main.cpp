97c57b875cc144899c6ed6a8aa5495048ebbafb8
liskovich <ricardsliskovskis@gmail.com>
2024-01-16 13:03:49 +0200

    complete: week2 hmw2

---
+++ week2_hmw2/main.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char user_input;
+  cin >> user_input;
+
+  switch (user_input)
+  {
+  case 'A':
+    cout << "Alfa" << endl;
+    break;
+  case 'B':
+    cout << "Bravo" << endl;
+    break;
+  case 'C':
+    cout << "Charlie" << endl;
+    break;
+  case 'D':
+    cout << "Delta" << endl;
+    break;
+  case 'E':
+    cout << "Echo" << endl;
+    break;
+  case 'F':
+    cout << "Foxtrot" << endl;
+    break;
+  case 'G':
+    cout << "Golf" << endl;
+    break;
+  case 'H':
+    cout << "Hotel" << endl;
+    break;
+  case 'I':
+    cout << "India" << endl;
+    break;
+  case 'J':
+    cout << "Juliett" << endl;
+    break;
+  case 'K':
+    cout << "Kilo" << endl;
+    break;
+  case 'L':
+    cout << "Lima" << endl;
+    break;
+  case 'M':
+    cout << "Mike" << endl;
+    break;
+  case 'N':
+    cout << "November" << endl;
+    break;
+  case 'O':
+    cout << "Oscar" << endl;
+    break;
+  case 'P':
+    cout << "Papa" << endl;
+    break;
+  case 'Q':
+    cout << "Queber" << endl;
+    break;
+  case 'R':
+    cout << "Romeo" << endl;
+    break;
+  case 'S':
+    cout << "Sierra" << endl;
+    break;
+  case 'T':
+    cout << "Tango" << endl;
+    break;
+  case 'U':
+    cout << "Uniform" << endl;
+    break;
+  case 'V':
+    cout << "Victor" << endl;
+    break;
+  case 'W':
+    cout << "Whiskey" << endl;
+    break;
+  case 'X':
+    cout << "Xray" << endl;
+    break;
+  case 'Y':
+    cout << "Yankee" << endl;
+    break;
+  case 'Z':
+    cout << "Zulu" << endl;
+    break;
+  
+  case '1':
+    cout << "One" << endl;
+    break;
+  case '2':
+    cout << "Two" << endl;
+    break;
+  case '3':
+    cout << "Three" << endl;
+    break;
+  case '4':
+    cout << "Four" << endl;
+    break;
+  case '5':
+    cout << "Five" << endl;
+    break;
+  case '6':
+    cout << "Six" << endl;
+    break;
+  case '7':
+    cout << "Seven" << endl;
+    break;
+  case '8':
+    cout << "Eight" << endl;
+    break;
+  case '9':
+    cout << "Nine" << endl;
+    break;
+  case '0':
+    cout << "Zero" << endl;
+    break;
+  default:
+    cout << "Not Recognized" << endl;
+    break;
+  }
+  return 0;
+}
