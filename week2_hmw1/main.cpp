69ed17e8c2b163aedf8b9528f49606740157d9f5
liskovich <ricardsliskovskis@gmail.com>
2024-01-16 12:47:16 +0200

    complete: week2 hmw1

---
+++ week2_hmw1/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b, c;
+  cin >> a >> b >> c;
+
+  if (a > b) {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+  if (b > c) {
+    int temp = b;
+    b = c;
+    c = temp;
+  }
+  if (a > b) {
+    int temp = a;
+    a = b;
+    b = temp;
+  }
+  cout << "Number 1: " << a << endl << "Number 2: " << b << endl << "Number 3: " << c << endl;  
+
+  return 0;
+}
